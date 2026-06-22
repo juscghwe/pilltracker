939441f56c85f31bb35ed5c6a8eb0712a25738c2
juscghwe <schweizer.julian@outlook.com>
2026-06-22 18:37:55 +0000

    backend: initialize backend with Express and setup API routes

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express from "express";
+
+import { router } from "./routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+export { app };
